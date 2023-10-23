d4c2734fad41fae0a4f61b6502a0eb8037ff728b
yun <yunyunpp96@outlook.com>
2023-10-23 14:49:08 +0800

    expose more params

--- R/table.R
+++ R/table.R
@@ -6,18 +6,28 @@
 #' @param x,y Axis value in \[0, 1\] of text in each cell of table.
 #' @param hjust,vjust Horizontal and vertical justification \[0, 1\] of text in
 #' each cell of table.
+#' @param row_scale_expand,col_scale_expand A vector of range expansion
+#' constants used to add some padding around the data to ensure that they are
+#' placed some distance away from the axes. See
+#' [scale_continuous][ggplot2::scale_x_continuous]. 
 #' @param row_header,col_header Axis value in \[0, 1\] of table header.
+#' @param row_header_position,col_header_position The position of the header,
+#' left or right for `col_header_position`, top or bottom for
+#' `row_header_position`.
 #' @param row_header_text,column_header_text A
 #' [element_text][ggplot2::element_text] object specifies the header attributes.
+#' @param clip Should drawing be clipped to the extent of the plot panel? A
+#' setting of "on" means yes, and a setting of "off" (the default) means no.
 #' @param add_band A bool, if `TRUE`, will add band interval in the background.
 #' @param band_col Color of background band interval.
 #' @return A [ggplot][ggplot2::ggplot] object.
 #' @export
 ggtable <- function(
     data, x = 0.5, y = 0.5, hjust = 0.5, vjust = 0.5,
-    row_header = 0.5, row_header_text = NULL,
-    col_header = 0.5, column_header_text = NULL,
-    add_band = TRUE, band_col = c("white", "#eff3f2")) {
+    row_scale_expand = c(0L, 0L), col_scale_expand = c(0L, 0L),
+    row_header = 0.5, row_header_position = NULL, row_header_text = NULL,
+    col_header = 0.5, col_header_position = NULL, column_header_text = NULL,
+    clip = "off", add_band = TRUE, band_col = c("white", "#eff3f2")) {
     if (is.matrix(data)) {
         data <- as.data.frame(data,
             make.names = FALSE,
@@ -28,11 +38,20 @@ ggtable <- function(
             "{.arg data} must be a {.cls data.frame} or {.cls matrix}"
         )
     }
+    row_header_position <- match.arg(row_header_position, c("bottom", "top"))
+    col_header_position <- match.arg(col_header_position, c("left", "right"))
     colnms <- rlang::names2(data)
+    names(data) <- colnms
     rownms <- rownames(data)
     rowids <- rownms %||% seq_len(nrow(data))
+    data <- dplyr::mutate(
+        data, dplyr::across(everything(), .fns = as.character)
+    )
     data$..__y__.. <- factor(rowids, levels = rowids)
-    data <- tidyr::pivot_longer(data, cols = !.data$..__y__..)
+    data <- tidyr::pivot_longer(data,
+        cols = !.data$..__y__..,
+        values_ptypes = character()
+    )
     data$y <- as.integer(data$..__y__..)
     data$x <- as.integer(factor(data$name, levels = colnms))
     data$hjust <- hjust
@@ -40,8 +59,8 @@ ggtable <- function(
     p <- ggplot2::ggplot(
         data = data,
         ggplot2::aes(
-            x = as.integer(.data$x) - 0.5 + .env$x,
-            y = .data$y - 0.5 + .env$y,
+            x = .data$x - 1L + .env$x,
+            y = .data$y - 1L + .env$y,
             hjust = .data$hjust,
             vjust = .data$vjust,
             label = .data$value
@@ -54,20 +73,29 @@ ggtable <- function(
         ggplot2::geom_text() +
         ggplot2::scale_x_continuous(
             name = NULL,
-            limits = range(data$x),
-            breaks = sort(unique(data$x)) - 0.5 + row_header,
-            labels = colnms
+            limits = c(0L, max(data$x)),
+            # breaks should be header coord value
+            breaks = seq_len(max(data$x)) - 1L + row_header,
+            # minor_breaks are the table separator line
+            minor_breaks = c(0L, seq_len(max(data$x))),
+            labels = colnms, expand = row_scale_expand,
+            position = row_header_position
         ) +
         ggplot2::scale_y_continuous(
             name = NULL,
-            breaks = sort(unique(data$y)) - 0.5 + col_header,
+            limits = c(0L, max(data$y)),
+            breaks = seq_len(max(data$y)) - 1L + col_header,
+            minor_breaks = c(0L, seq_len(max(data$y))),
             labels = rownms,
-            expand = rep_len(0L, 2L)
+            expand = col_scale_expand,
+            position = col_header_position
         ) +
+        ggplot2::coord_cartesian(clip = clip) +
         ggplot2::theme(
             axis.ticks = ggplot2::element_blank(),
             axis.text.x = row_header_text %||% ggplot2::element_text(),
-            axis.text.y = column_header_text %||% ggplot2::element_text()
+            axis.text.y = column_header_text %||% ggplot2::element_text(),
+            panel.grid.major = ggplot2::element_blank()
         )
 }
 
@@ -76,24 +104,24 @@ ggadd_band <- function(band_col = c("white", "#eff3f2")) {
         # lowest band
         ggplot2::geom_rect(ggplot2::aes(
             xmin = -Inf, xmax = Inf,
-            ymin = -Inf,
-            ymax = min(.data$y) - 0.5,
+            ymin = -Inf, ymax = 0L,
             fill = "0"
         )),
         # plot band
         ggplot2::geom_rect(ggplot2::aes(
             xmin = -Inf, xmax = Inf,
-            ymin = .data$y - 0.5,
-            ymax = .data$y + 0.5,
+            ymin = .data$y - 1, ymax = .data$y,
             fill = factor(.data$y %% 2L)
         )),
         # highest band
         ggplot2::geom_rect(ggplot2::aes(
             xmin = -Inf, xmax = Inf,
-            ymin = max(.data$y) + 0.5,
+            ymin = max(.data$y),
             ymax = Inf,
             fill = "1"
         )),
         ggplot2::scale_fill_manual(values = band_col, guide = "none")
     )
 }
+
+utils::globalVariables("everything")
