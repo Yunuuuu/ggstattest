21c8555017ea47250e65e0a429c08321fe5dccba
yun <yunyunpp96@outlook.com>
2023-10-23 21:05:23 +0800

    fix R CMD check error

--- R/import-standalone-assert.R
+++ R/import-standalone-assert.R
@@ -265,26 +265,6 @@ assert_data_frame_columns <- function(x, columns, ..., args = rlang::caller_arg(
     }
 }
 
-# S4 object -------------------------------------
-assert_s4_class <- function(
-    x, is_class, what, ...,
-    arg = rlang::caller_arg(x),
-    call = rlang::caller_env()) {
-    if (rlang::is_string(is_class)) {
-        class <- is_class
-        is_class <- function(x) {
-            methods::is(x, class)
-        }
-        if (missing(what)) {
-            what <- class
-        }
-    }
-    assert_(
-        x = x, assert_fn = is_class, what = what,
-        ..., arg = arg, call = call
-    )
-}
-
 #' Report if an argument has specific length
 #' @keywords internal
 #' @noRd
