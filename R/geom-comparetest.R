0cd5a9cac9e8dcd838165686e42aa5f101ac1ec5
Yunuuuu <yunyunpp96@outlook.com>
2024-12-17 00:15:25 +0800

    fix warning no label_colour and segment_colour argument

--- R/geom-comparetest.R
+++ R/geom-comparetest.R
@@ -13,8 +13,10 @@
 #' the number of panel. Using [rel][ggplot2::rel] to signal values as the
 #' fraction of the difference between the horizontal segment and the maximal
 #' value of current group.  Default: `rel(0.01)`
-#' @param label_color,segment_color Color for the label and segment lines, if
+#' @param label_colour,segment_colour Color for the label and segment lines, if
 #' `NULL`, will use the mapped color.
+#' @param label_color,segment_color Alias for `label_colour` and
+#' `segment_colour`.
 #' @param nudge_x,nudge_y Horizontal and vertical adjustment to nudge labels by.
 #'   Useful for offsetting text from segments, particularly on discrete scales.
 #'   A list or a numeric vector, this will be recycled to fit the number of
@@ -93,8 +95,7 @@ geom_comparetest <- function(mapping = NULL, data = NULL,
                              stat = "comparetest", position = "identity",
                              height = NULL, step_increase = NULL,
                              tip_length = NULL,
-                             label_color = NULL,
-                             segment_color = NULL,
+                             label_colour = NULL, segment_colour = NULL,
                              nudge_x = 0, nudge_y = 0,
                              ...,
                              parse = FALSE, arrow = NULL, arrow_fill = NULL,
@@ -102,7 +103,9 @@ geom_comparetest <- function(mapping = NULL, data = NULL,
                              na.rm = FALSE,
                              orientation = NA,
                              show.legend = NA,
-                             inherit.aes = TRUE) {
+                             inherit.aes = TRUE,
+                             label_color = NULL,
+                             segment_color = NULL) {
     ggplot2::layer(
         data = data,
         mapping = mapping,
@@ -122,8 +125,8 @@ geom_comparetest <- function(mapping = NULL, data = NULL,
             arrow_fill = arrow_fill,
             lineend = lineend,
             linejoin = linejoin,
-            label_color = label_color,
-            segment_color = segment_color,
+            label_colour = label_colour %||% label_color,
+            segment_colour = segment_colour %||% segment_color,
             na.rm = na.rm,
             orientation = orientation,
             ...
@@ -254,7 +257,7 @@ GeomComparetest <- ggplot2::ggproto("GeomComparetest", ggplot2::Geom,
     },
     draw_panel = function(self, data, panel_params, coord,
                           tip_length = NULL,
-                          label_color = NULL, segment_color = NULL,
+                          label_colour = NULL, segment_colour = NULL,
                           parse = FALSE, arrow = NULL, arrow_fill = NULL,
                           lineend = "butt", linejoin = "round",
                           na.rm, flipped_aes = FALSE) {
@@ -354,11 +357,11 @@ GeomComparetest <- ggplot2::ggproto("GeomComparetest", ggplot2::Geom,
         if (flipped_aes) {
             label_data$angle <- label_data$angle - 90
         }
-        if (!is.null(label_color)) {
-            seg_data$color <- label_color
+        if (!is.null(label_colour)) {
+            label_data$colour <- label_colour
         }
-        if (!is.null(segment_color)) {
-            seg_data$color <- segment_color
+        if (!is.null(segment_colour)) {
+            seg_data$colour <- segment_colour
         }
         grid::gList(
             # draw label
